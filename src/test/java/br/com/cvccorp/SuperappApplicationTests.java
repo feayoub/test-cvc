2ef33f6de053e6a419cfc843445d5aa8c55e0593
Felipe <fayoub94@hotmail.com>
2020-07-01 09:13:31 -0300

    Initial commit

---
+++ src/test/java/br/com/cvccorp/SuperappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cvccorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
